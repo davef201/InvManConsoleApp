b5dd7ea4e6a44e41468dfd4884cb717fdd023d9d
David Forshaw <davidbforshaw@outlook.com>
2020-02-24 09:14:21 +0000

    Added unit tests for Name and SellIn

--- InvManConsoleAppTest/UnitTests/ItemNameTest.cs
+++ InvManConsoleAppTest/UnitTests/ItemNameTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using InvManConsoleApp.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,20 @@ namespace InvManConsoleAppTest.UnitTests
     [TestClass]
     public class ItemNameTest
     {
+        [TestMethod]
+        public void UpdateName_Fresh_SameValue()
+        {
+            Item item = new FreshItem { Name = "a" };
+            item.UpdateName();
+            Assert.AreEqual("a", item.Name);
+        }
+
+        [TestMethod]
+        public void UpdateName_InvalidItem_NoSuchItem()
+        {
+            Item item = new InvalidItem { Name = "a" };
+            item.UpdateName();
+            Assert.AreEqual("NO SUCH ITEM", item.Name);
+        }
     }
 }
--- InvManConsoleAppTest/UnitTests/ItemSellInTest.cs
+++ InvManConsoleAppTest/UnitTests/ItemSellInTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using InvManConsoleApp.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,34 @@ namespace InvManConsoleAppTest.UnitTests
     [TestClass]
     public class ItemSellInTest
     {
+        [TestMethod]
+        public void UpdateSellIn_Fresh_ReduceBy1()
+        {
+            Item item = new FreshItem();
+            item.SellIn = 1;
+            item.UpdateSellIn();
+            Assert.AreEqual(0, item.SellIn);
+
+        }
+
+        [TestMethod]
+        public void UpdateSellIn_Soap_SameValue()
+        {
+            Item item = new Soap();
+            item.SellIn = 1;
+            item.UpdateSellIn();
+            Assert.AreEqual(1, item.SellIn);
+
+        }
+
+        [TestMethod]
+        public void UpdateSellIn_InvalidItem_Null()
+        {
+            Item item = new InvalidItem();
+            item.SellIn = 1;
+            item.UpdateSellIn();
+            Assert.IsNull(item.SellIn);
+
+        }
     }
 }
